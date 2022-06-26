4b4f6f85d9d65f8a5d7e2976b224cfdeb5f7d10d
Zarak Ansari <2711430a@student.gla.ac.uk>
2022-06-26 21:27:59 +0100

    First Commit

---
+++ src/test/java/com/mscproject/startup/StartupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscproject.startup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
